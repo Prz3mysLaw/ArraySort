396972b4e3b88f2d96e2b6a1cc20931148192f34
bboyKamel <nacoon@tlen.pl>
2019-02-11 20:01:24 +0100

    create insercionSort funkcion

--- src/main/java/SortUtil.java
+++ src/main/java/SortUtil.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class SortUtil {
+
+    public static int[] insercionSort(int[] initial){
+        int[] ret = initial.clone();
+
+        for (int i = 0; i < ret.length; i++) {
+            Pair min = ArrayUtil.findMinimumAndIndex(i, ret);
+
+            int temp = ret[i];
+            ret[i] = min.getValue();
+            ret[min.getIndex()] = temp;
+        }
+        return ret;
+    }
 }
